05dfc14ce78c82588d6a542b92423a522cf51fda
wjc708 <wangjunchen@buaa.edu.cn>
2025-01-20 21:12:13 +0800

    add task_node package for individual tasks

---
+++ task_node/src/task_node.cpp
@@ -0,0 +1,50 @@
+#include <chrono>
+#include <errno.h>
+#include <memory>
+#include <string>
+#include <thread>
+#include <atomic>
+#include "rclcpp/executors.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "realtime_tools/realtime_helpers.hpp"
+
+using namespace std::chrono_literals;
+
+void *(*control_func)(void *) = nullptr;
+int main(int argc, char **argv)
+{
+    rclcpp::init(argc, argv);
+    int kSchedPriority = 50;
+    std::shared_ptr<rclcpp::Executor> executor =
+        std::make_shared<rclcpp::executors::MultiThreadedExecutor>();
+
+    auto node = std::make_shared<rclcpp::Node>("task_node");
+    auto ret = realtime_tools::lock_memory();
+    if (!ret.first)
+        RCLCPP_WARN(node->get_logger(), "Unable to lock the memory : '%s'", ret.second.c_str());
+
+    auto thread = std::make_shared<std::thread>(
+        [node, kSchedPriority]()
+        {
+            if (!realtime_tools::configure_sched_fifo(kSchedPriority))
+            {
+                RCLCPP_WARN(node->get_logger(), "Could not enable FIFO RT scheduling policy");
+            }
+            else
+            {
+                RCLCPP_INFO(node->get_logger(), "Successful set up FIFO RT scheduling policy with priority %i.",
+                            kSchedPriority);
+            }
+            // while (rclcpp::ok())
+            // {
+
+            // }
+            if(control_func)
+                control_func(nullptr);
+        });
+    executor->add_node(node);
+    executor->spin();
+    thread->join();
+    rclcpp::shutdown();
+    return 0;
+}
