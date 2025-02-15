b7067eb2cb2e9cc3f880c529e180f506769dc8dc
Eric Laptop <buaalh@buaa.edu.cn>
2025-02-15 09:56:13 +0800

    remove unnecessary namespaces in .h & .hpp files

--- ros2_utility/include/ros2_utility/transform_interpolator.hpp
+++ ros2_utility/include/ros2_utility/transform_interpolator.hpp
@@ -11,18 +11,14 @@
 
 #include "robot_math/robot_math.hpp"
 
-using namespace std;
-using namespace Eigen;
-using namespace robot_math;
-
 class TransformInterpolator
 {
 public:
     // 构造函数
-    TransformInterpolator(const vector<Matrix4d> &T_traj, double k = 0.1, double dt = 0.002, bool print_detail = false);
+    TransformInterpolator(const std::vector<Eigen::Matrix4d> &T_traj, double k = 0.1, double dt = 0.002, bool print_detail = false);
 
     // 追加一个变换矩阵
-    void addTransform(const Matrix4d &T);
+    void addTransform(const Eigen::Matrix4d &T);
 
     // 追加正弦轨迹，生成在xoy平面，并支持正弦波大小比例调节
     void appendSineTrajectory(int total_sine_points, double amplitude, double period);
@@ -34,10 +30,10 @@ public:
     bool isInterpolationComplete() const;
 
     // 执行一次插值计算，返回当前插值的变换矩阵
-    Matrix4d step();
+    Eigen::Matrix4d step();
 
     // 执行一次插值计算，同时设置速度比例
-    Matrix4d step(double k);
+    Eigen::Matrix4d step(double k);
 
     // 设置下一个轨迹段
     bool setNextTransform();
@@ -46,20 +42,20 @@ public:
     size_t getCurrentIndex() const;
 
     // S型轨迹插值4个点
-    void append4Transform(const Matrix4d &Tdip);
+    void append4Transform(const Eigen::Matrix4d &Tdip);
 
     // 将 T_traj_ 的后 num 个齐次变换矩阵乘以 Tdip 倒序追加到 T_traj_ 末尾
-    void appendNumTransform(int num, const Matrix4d &Tdip);
+    void appendNumTransform(int num, const Eigen::Matrix4d &Tdip);
 
 // private:
-    vector<Matrix4d> T_traj_; // 轨迹点集合
+    std::vector<Eigen::Matrix4d> T_traj_; // 轨迹点集合
     double k_;                // 当前速度比例
     double dt_;               // 时间步长
     bool print_detail_;       // 是否打印详细信息
     size_t current_index_;    // 当前轨迹段的索引
     double t_;                // 当前插值因子
     double distance_;         // 起始到目标的距离
-    Vector6d V_;              // T1 到 T2 的对数映射
+    Eigen::Vector6d V_;              // T1 到 T2 的对数映射
 };
 
 #endif // TRANSFORM_INTERPOLATOR
