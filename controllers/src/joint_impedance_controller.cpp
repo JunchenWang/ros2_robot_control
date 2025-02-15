b49c77aab0f1df6fb6ac2086c4ae940a57464f80
Eric Laptop <buaalh@buaa.edu.cn>
2025-02-15 14:27:37 +0800

    add data_comm in ros2_utility package, add parameter callback in joint impedance controller, to be tested

--- controllers/src/joint_impedance_controller.cpp
+++ controllers/src/joint_impedance_controller.cpp
@@ -1,9 +1,13 @@
 #include "controller_interface/controller_interface.hpp"
+#include "rcl_interfaces/msg/set_parameters_result.hpp"
+#include "realtime_tools/realtime_box.hpp"
 #include "robot_math/MovingFilter.h"
 #include "robot_math/robot_math.hpp"
+#include "ros2_utility/data_comm.hpp"
 #include "ros2_utility/data_logger.hpp"
 #include "ros2_utility/file_utils.hpp"
 #include <iostream>
+
 using namespace robot_math;
 namespace controllers
 {
@@ -11,22 +15,38 @@ namespace controllers
     {
     public:
         JointImpedanceController() {}
-        ~JointImpedanceController()
-        {
-        }
+        ~JointImpedanceController() {}
 
         CallbackReturn on_configure(const rclcpp_lifecycle::State & /*previous_state*/) override
         {
             dof_ = robot_->dof;
             node_->get_parameter_or<std::vector<double>>("K", K_vec_, {600.0, 600.0, 600.0, 600.0, 250.0, 150.0, 50.0});
             node_->get_parameter_or<std::vector<double>>("B", B_vec_, {50.0, 50.0, 50.0, 50.0, 30.0, 25.0, 15.0});
-            K_ = Eigen::Map<Eigen::VectorXd>(K_vec_.data(), dof_);
-            B_ = Eigen::Map<Eigen::VectorXd>(B_vec_.data(), dof_);
+            K_in_box_.set(K_vec_);
+            B_in_box_.set(B_vec_);
+            parameters_callback_handle_ = node_->add_on_set_parameters_callback(
+                [&](std::vector<rclcpp::Parameter> parameters) -> rcl_interfaces::msg::SetParametersResult
+                {
+                    RCLCPP_INFO(node_->get_logger(), "Parameter %s update requested.", parameters[0].get_name().c_str());
+                    for (const auto &parameter : parameters)
+                    {
+                        if (parameter.get_name() == "K")
+                            K_in_box_.set([=](auto &value)
+                                          { value = parameter.as_double_array(); });
+                        else if (parameter.get_name() == "B")
+                            B_in_box_.set([=](auto &value)
+                                          { value = parameter.as_double_array(); });
+                    }
+                    auto result = rcl_interfaces::msg::SetParametersResult();
+                    result.successful = true;
+                    return result;
+                });
             return CallbackReturn::SUCCESS;
         }
 
         CallbackReturn on_activate(const rclcpp_lifecycle::State & /*previous_state*/)
         {
+            DataComm::getInstance()->setDestAddress("127.0.0.1", 7755);
             time_ = 0;
             const std::vector<double> &q_vec = state_->get<double>("position");
             qd_ = Eigen::Map<const Eigen::VectorXd>(q_vec.data(), dof_).eval();
@@ -50,8 +70,8 @@ namespace controllers
                     DATA_WRAPPER(c_cal_),
                 },
                 {
-                    CONFIG_WRAPPER(K_),
-                    CONFIG_WRAPPER(B_),
+                    CONFIG_WRAPPER(K_vec_),
+                    CONFIG_WRAPPER(B_vec_),
                 },
                 1000);
             return CallbackReturn::SUCCESS;
@@ -84,6 +104,13 @@ namespace controllers
             Eigen::Map<const Eigen::VectorXd> dq(dq_vec.data(), dof_);
             Eigen::Map<const Eigen::VectorXd> c(c_vec.data(), dof_);
 
+            K_in_box_.try_get([=](auto const &value)
+                              { K_vec_ = value; });
+            B_in_box_.try_get([=](auto const &value)
+                              { B_vec_ = value; });
+            K_ = Eigen::Map<Eigen::VectorXd>(K_vec_.data(), dof_);
+            B_ = Eigen::Map<Eigen::VectorXd>(B_vec_.data(), dof_);
+
             m_c_g_matrix(robot_, q_vec, dq_vec, M_, C_, g_, Jb_, dJb_, dM_, dTb_, Tb_);
             c_cal_ = C_ * dq; // 验证C_ * dq与机器人提供的c是否一致
 
@@ -97,11 +124,18 @@ namespace controllers
             tau_d_ = tau_d;
             tau_cmd_ = tau_cmd;
 
+            log2Channel(robot_data_, 0, q.data(), dof_);
+            log2Channel(robot_data_, 1, dq.data(), dof_);
+            log2Channel(robot_data_, 2, c.data(), dof_);
+            log2Channel(robot_data_, 3, c_cal_.data(), dof_);
+            robot_data_.t = time_;
+            DataComm::getInstance()->sendRobotStatus(robot_data_);
             cal_time_ = 1e-6 * std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - start_time).count();
             data_logger_->record();
         }
 
     protected:
+        rclcpp::node_interfaces::OnSetParametersCallbackHandle::SharedPtr parameters_callback_handle_;
         int dof_;
         Eigen::MatrixXd M_, C_, Jb_, dJb_, dM_;
         Eigen::VectorXd g_, q_, dq_, c_, c_cal_;
@@ -110,9 +144,11 @@ namespace controllers
         Eigen::VectorXd tau_cmd_, tau_d_, tau_ext_;
         Eigen::VectorXd qd_, dqd_, ddqd_, qe_, dqe_;
         double success_rate_, cal_time_;
+        realtime_tools::RealtimeBox<std::vector<double>> K_in_box_, B_in_box_;
         std::vector<double> K_vec_, B_vec_;
         DataLogger *data_logger_;
         double time_;
+        RobotData robot_data_;
     };
 } // namespace controllers
 
