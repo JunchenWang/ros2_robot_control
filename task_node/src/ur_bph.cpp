1a293b3f5319b6df52687d9f15cc34c4d3053934
wjc <wangjunchen@buaa.edu.cn>
2026-06-22 17:04:12 +0800

    update

--- task_node/src/ur_bph.cpp
+++ task_node/src/ur_bph.cpp
@@ -7,6 +7,7 @@
 #include "rclcpp/executors.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "realtime_tools/realtime_helpers.hpp"
+#include "robot_math/robot_math.hpp"
 // this is a template
 using namespace std::chrono_literals;
 
@@ -17,7 +18,10 @@ int main(int argc, char **argv)
     std::shared_ptr<rclcpp::Executor> executor =
         std::make_shared<rclcpp::executors::MultiThreadedExecutor>();
 
-    auto node = std::make_shared<rclcpp::Node>("task_node");
+    rclcpp::NodeOptions node_options;
+    node_options.allow_undeclared_parameters(true);
+    node_options.automatically_declare_parameters_from_overrides(true);
+    auto node = std::make_shared<rclcpp::Node>("ur_bph", "", node_options);
     auto ret = realtime_tools::lock_memory();
     if (!ret.first)
         RCLCPP_WARN(node->get_logger(), "Unable to lock the memory : '%s'", ret.second.c_str());
@@ -36,6 +40,10 @@ int main(int argc, char **argv)
             }
             // for calculating sleep time
             // double dt = 1.0 / update_rate_;
+            auto robot_description = node->get_parameter_or<std::string>("robot_description", "");
+            std::vector<std::string> jt_names;
+            std::string base, fl;
+            auto robot = robot_math::urdf_to_robot(robot_description, jt_names, fl, base);
             bool first_loop = true;
             int update_rate = 500;
             auto const period = std::chrono::nanoseconds(1'000'000'000 / update_rate);
@@ -51,11 +59,16 @@ int main(int argc, char **argv)
                     first_loop = false;
                 else
                     measured_period = current_time - previous_time, total_time += measured_period.seconds();
-
                 previous_time = current_time;
                 
                 // execute update loop
                 RCLCPP_INFO(node->get_logger(),  "%f sec.", measured_period.seconds());
+                auto P2 = Eigen::Vector3d(0, -0.3, 0.108);
+                auto P1 = Eigen::Vector3d(0, 0, 0.108);
+                
+
+
+
                 // wait until we hit the end of the period
                 next_iteration_time += period;
                 const auto steady_now = std::chrono::steady_clock::now();
